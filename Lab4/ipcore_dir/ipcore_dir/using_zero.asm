f0db44c09d957237409f8413e23dfdeac2fcf28d
xudenden <56295612@qq.com>
2020-07-10 14:58:23 +0800

    add more MIPS source, rename my1 to using_t and add using_zero

--- Lab4/ipcore_dir/ipcore_dir/using_zero.asm
+++ Lab4/ipcore_dir/ipcore_dir/using_zero.asm
@@ -2,16 +2,8 @@ lw  $at, 0($zero)
 lw  $v0, 12($zero)
 lw  $a0, 4($zero)
 add $v1, $at, $v0
-
-
 add $v0, $v1, $v0
 add $v1, $v1, $at
-# add $t5, $v1, $v0
-# or $v0, $zero, $t5
-# add $t5, $v1, $at
-# or $v1, $zero, $t5
-
-
 beq $v1, $a0, 4
 j   16
 sw  $v0, 8($zero)
